13883c885222dfddee449f0639a855e4559ceedc
Ana Clara Campana <anacampana@cpejr.com.br>
2019-09-25 16:18:38 -0300

    adicionei o schema do funcionario e master

---
+++ smart-management/models/user.js
@@ -0,0 +1,111 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+
+  username: String,
+  type: {
+    type: String,
+    enum:['Master','Funcionario']
+  },
+  uid:String
+
+}, { timestamps: true, static: false });
+
+const UserModel = mongoose.model('User', userSchema);
+
+class User {
+  /**
+   * Get all Users from database
+   * @returns {Array} Array of Users
+   */
+  static getAll() {
+    return new Promise((resolve, reject) => {
+      UserModel.find({}).exec().then((results) => {
+        resolve(results);
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  }
+
+  /**
+   * Get a User by it's id
+   * @param {string} id - User Id
+   * @returns {Object} - User Document Data
+   */
+  static getById(id) {
+    return new Promise((resolve, reject) => {
+      UserModel.findById(id).exec().then((result) => {
+        resolve(result);
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  }
+
+  /**
+   * Create a new User
+   * @param {Object} user - User Document Data
+   * @returns {string} - New User Id
+   */
+  static create(user) {
+    return new Promise((resolve, reject) => {
+      UserModel.create(user).then((result) => {
+        resolve(result._id);
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  }
+
+  /**
+   * Update a User
+   * @param {string} id - User Id
+   * @param {Object} User - User Document Data
+   * @returns {null}
+   */
+  static update(id, user) {
+    return new Promise((resolve, reject) => {
+      UserModel.findByIdAndUpdate(id, user).then(() => {
+        resolve();
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  }
+
+  /**
+  * Delete a User
+  * @param {string} id - User Id
+  * @returns {null}
+  */
+
+  static delete(id) {
+    return new Promise((resolve, reject) => {
+      UserModel.findOneAndDelete({_id: id}).then(() => {
+        resolve();
+      }).catch((err) => {
+        reject(err);
+      });
+   });
+ }
+
+  /**
+   * Get a User by it's uid
+   * @param {string} id - User Uid
+   * @returns {Object} - User Document Data
+   */
+  static getByUid(id) {
+    return new Promise((resolve, reject) => {
+      UserModel.findOne({ uid: id }).exec().then((result) => {
+        resolve(result);
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  }
+
+
+  }
+
+module.exports = User;
--- smart-management/routes/index.js
+++ smart-management/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require('firebase');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('signup', { title: 'Cadastro' });
+});
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: 'Login' });
+});
+router.post('/login', function(req, res, next) {
+  const user=req.body.user;
+  firebase.auth().signInWithEmailAndPassword(user.username, user.password).then((userF)=>{
+console.log("deu certoooooooooooooooooooooooooooooooooooooooooooooooo");
+res.redirect('/login')
+
+}).catch((error)=>{
+  console.log(error);
+  res.redirect('/error')
+})
+
+
+
+
 });
 
 module.exports = router;
