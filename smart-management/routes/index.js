2856c1b367e151019e9fdf24e1be3cd0f2bdc3c4
Hermon <hermon1998@hotmail.com>
2019-10-02 11:30:52 -0300

    Home

--- smart-management/routes/index.js
+++ smart-management/routes/index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Home' });
+});
+router.get('/signup', function(req, res, next) {
   res.render('signup', { title: 'Cadastro' });
 });
 
