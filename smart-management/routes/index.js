bee7ecf636f372b1574c679c6542a057b1a2e735
Hermon <hermon1998@hotmail.com>
2019-10-09 10:47:35 -0300

    Erros do merge corrigidos

--- smart-management/routes/index.js
+++ smart-management/routes/index.js
@@ -18,7 +18,7 @@ router.post('/login', function(req, res, next) {
   firebase.auth().signInWithEmailAndPassword(user.username, user.password).then((userF)=>{
     mongo.getByUid(userF.user.uid).then((result)=> {
       if(result.type=='Master'){
-        res.redirect('/');
+        res.redirect('/signup');
       }
       else{
         res.redirect('/acompanhamento');
